274fe5c8120ac9056fded52774a0f02b337f19cb
Tom Molesworth <tom@perlsite.co.uk>
2014-08-31 16:54:05 +0000

    Show correct line
    git-svn-id: http://entitymodel.com/repo/trunk/Tickit/Tickit-Widget-FileViewer@867 c09ddf91-2ef0-45e8-8c68-47ccf8e3121d

--- lib/Tickit/Widget/FileViewer.pm
+++ lib/Tickit/Widget/FileViewer.pm
@@ -8,7 +8,7 @@ use Tickit::Utils qw(substrwidth);
 use List::Util qw(min max);
 use Text::Tabs ();
 
-our $VERSION = '0.003';
+our $VERSION = '0.004';
 
 =head1 NAME
 
@@ -222,6 +222,18 @@ sub cursor_line {
 		return $self;
 	}
 	return $self->{cursor_line};
+}
+
+sub window_gained {
+	my ($self, $win) = @_;
+	$self->SUPER::window_gained($win);
+	my $line = $self->cursor_line;
+	if($line < $self->top_line) {
+		$self->top_line($line);
+	} elsif($line >= $self->top_line + $win->lines) {
+		$self->top_line($line - ($win->lines - 1));
+	}
+	$self->redraw;
 }
 
 =head2 handle_key
